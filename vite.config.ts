32675a1d6875e09f358bfb25d934306871cb9f26
user <gktin@yandex.ru>
2024-01-26 05:05:31 +0400

    add types alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	publicDir: '../static',
+	resolve: {
+		alias: {
+			'src/types': './src/types',
+		},
+	},
 	root: './src/client',
 	server: {
 		open: true,
