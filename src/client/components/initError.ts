f448708928d8a3f10c4562bacc76c091330e3a83
user <gktin@yandex.ru>
2024-01-29 03:11:37 +0400

    error colors

--- src/client/components/initError.ts
+++ src/client/components/initError.ts
@@ -1,17 +1,17 @@
 export function initError (view: HTMLElement) {
 	const el = document.createElement('pre');
 
-	el.classList.add('view_container_error');
+	el.classList.add('error');
 
 	const setError = (err: unknown) => {
 		if (!err) {
-			el.classList.remove('state_view');
+			el.classList.remove('view');
 
 			return;
 		}
 
 		el.innerHTML = JSON.stringify(err, null, '\t');
-		el.classList.add('state_view');
+		el.classList.add('view');
 	};
 
 	view.appendChild(el);
