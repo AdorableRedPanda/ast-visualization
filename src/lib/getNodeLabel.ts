2ee2a6b12f7d5bf4a5e4df65157894f2dbc4496a
user <gktin@yandex.ru>
2024-01-30 09:07:04 +0400

    simplify labels

--- src/lib/getNodeLabel.ts
+++ src/lib/getNodeLabel.ts
@@ -3,31 +3,17 @@ import { Node, isIdentifier } from '@babel/types';
 
 export const getNodeLabel = (node: Node) => {
 	switch (node.type) {
-		case 'ClassProperty':
-		case 'ClassMethod':
-			const name = generate(node.key).code;
-
-			return `${node.type}\n${name}`;
-		case 'ClassDeclaration':
-			const id = isIdentifier(node.id) ? node.id.name : '';
-
-			return `${node.type}\n${id}`;
 		case 'BinaryExpression':
 		case 'UnaryExpression':
 			return `Operator: ${node.operator}`;
 		case 'JSXIdentifier':
 		case 'Identifier':
 			return node.name;
-		case 'MemberExpression':
-		case 'ObjectProperty':
-			return node.type;
 		case 'JSXText':
 		case 'StringLiteral':
 			return node.value;
 		case 'NumericLiteral':
 			return node.value.toString(10);
-		case 'JSXElement':
-			return generate(node.openingElement.name).code;
 	}
 
 	return node.type;
